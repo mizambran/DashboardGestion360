3cd03d96973638ad9e23315675f08d7e29ced9c4
Miguel Angel Zambrano <angel245687@gmail.com>
2026-01-24 12:32:27 -0300

    creo cards en Home para conexiones

---
+++ src/Footer.jsx
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+const Footer = () => {
+  return (
+    <div>
+       <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Desarrollado por Miguel Angel</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Sigueme en: <a href="https://www.instagram.com/miguel.zambran0?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target='_blank'>Instagram</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,46 @@
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router';
+import { AiFillAndroid } from "react-icons/ai";
 
 const Home = () => {
   return (
-    <div>
-      
+    <>
+    <div className='container d-flex justify-content-between'>
+    <Card className="text-center col-4 col-md-3 my-5">
+      <Card.Header>Basic</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button as={Link} to={'./dashboardApi'} variant="primary">API</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className="text-center col-4 col-md-3 my-5">
+      <Card.Header>Basic</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button as={Link} to={'./dashboardSql'} variant="primary">SQL</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className="text-center col-4 col-md-3 my-5">
+      <Card.Header>Premium</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button as={Link} to={'./dashboardArchivo'} variant="primary"> <AiFillAndroid /> Archivo</Button>
+      </Card.Body>
+    </Card>
     </div>
+    </>
   )
 }
 
